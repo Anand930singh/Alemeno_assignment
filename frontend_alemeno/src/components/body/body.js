ebd07647c75b079f7e2d0affdc6af50f342e6053
Anand930singh <anand2002king@gmail.com>
2024-06-15 00:52:39 +0530

    frontend improved

--- frontend_alemeno/src/components/body/body.js
+++ frontend_alemeno/src/components/body/body.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './body.css';
 import Download from '../../assets/download.png';
-import { JsonView, allExpanded, darkStyles} from 'react-json-view-lite';
+import { JsonView, allExpanded, darkStyles } from 'react-json-view-lite';
 import "react-json-view-lite/dist/index.css";
 
 function Body() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [responseData, setResponseData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // Function to handle file selection
   const handleImageUpload = (e) => {
@@ -21,11 +22,12 @@ function Body() {
       return;
     }
 
+    setLoading(true); // Set loading to true before making the request
+
     const formData = new FormData();
     formData.append('image', selectedImage);
-    console.log('hii')
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/urineStrip/', {
+      const response = await fetch('https://alemeno-assignment-qd7t.onrender.com/urineStrip/', {
         method: 'POST',
         body: formData,
       });
@@ -38,30 +40,34 @@ function Body() {
       }
     } catch (error) {
       console.error('Error:', error);
+    } finally {
+      setLoading(false); // Set loading to false after the request completes
     }
   };
 
   return (
     <div className='container'>
-    <div className='bodyContain'>
-      <div className='leftBody'>
-        <label htmlFor="uploadInput">
-          <img className='uploadIcon' src={Download} alt="Upload Image" width='80' height='80' />
-        </label>
-        <input
-          id="uploadInput"
-          type="file"
-          accept="image/*"
-          style={{ display: 'none' }}
-          onChange={handleImageUpload}
-        />
-        <div className='tagline'>Upload Your Urine Strip Image</div>
-        {selectedImage && (
-          <div className="selectedImageName">{selectedImage.name}</div>
-        )}
-        <button className='button-3' onClick={handleSubmit}>Submit</button>
-      </div>
-      <div className='rightBody'>
+      <div className='bodyContain'>
+        <div className='leftBody'>
+          <label htmlFor="uploadInput">
+            <img className='uploadIcon' src={Download} alt="Upload Image" width='80' height='80' />
+          </label>
+          <input
+            id="uploadInput"
+            type="file"
+            accept="image/*"
+            style={{ display: 'none' }}
+            onChange={handleImageUpload}
+          />
+          <div className='tagline'>Upload Your Urine Strip Image</div>
+          {selectedImage && (
+            <div className="selectedImageName">{selectedImage.name}</div>
+          )}
+          <button className='button-3' onClick={handleSubmit} disabled={loading}>
+            {loading ? <div className="loader"></div> : 'Submit'}
+          </button>
+        </div>
+        <div className='rightBody'>
           <div className='imgContain'>
             {selectedImage ? (
               <img src={URL.createObjectURL(selectedImage)} alt="Selected" />
@@ -73,21 +79,21 @@ function Body() {
                 key={index}
                 className="rgbText"
                 style={{
-                  top: `${position.coordinates[1]/3.6}px`,
-                  left: `${position.coordinates[0]/2.1}px`,
+                  top: `${position.coordinates[1] / 3.6}px`,
+                  left: `${position.coordinates[0] / 2.1}px`,
                 }}
               >
                 {`RGB: (${position.rgb.join(', ')})`}
               </div>
             ))}
           </div>
+        </div>
       </div>
-    </div>
-    <div className='jsonViewer'>
-    {responseData && responseData.contour_labels && (
-        <JsonView data={responseData.contour_labels} shouldExpandNode={allExpanded} style={darkStyles} />
-    )}
-    </div>
+      <div className='jsonViewer'>
+        {responseData && responseData.contour_labels && (
+          <JsonView data={responseData.contour_labels} shouldExpandNode={allExpanded} style={darkStyles} />
+        )}
+      </div>
     </div>
   );
 }
